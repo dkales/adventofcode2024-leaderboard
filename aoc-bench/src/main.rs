99f35a516fd2d8f39f5cba2fc79b106907e45da7
Daniel Kales <11509575+dkales@users.noreply.github.com>
2023-12-15 12:36:01 +0100

    feat: day15

--- aoc-bench/src/main.rs
+++ aoc-bench/src/main.rs
@@ -388,8 +388,8 @@ const INPUTS_OUTPUTS: [(u8, &'static str, &'static str, &'static str); 25] = [
     (
         15,
         include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/inputs/day15.txt")),
-        "",
-        "",
+        "517315",
+        "247763",
     ),
     (
         16,
